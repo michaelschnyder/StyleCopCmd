7691e0f63fd5ff7a0505e3839800dc59f03c67fe
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-01-20 12:03:42 +0100

    Naming adjustments

--- StyleCopCmd/Reporter/TeamCity/TeamCityMessageReporter.cs
+++ StyleCopCmd/Reporter/TeamCity/TeamCityMessageReporter.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 using JetBrains.TeamCity.ServiceMessages.Write.Special;
 
@@ -16,33 +14,33 @@ namespace StyleCopCmd.Reporter.TeamCity
 {
     /// <summary>
     /// Reports StyleCop issues to TeamCity with Service Messages
-    /// See http://blog.jonnyzzz.name/2012/12/teeamcityservicemessages-v30.html and 
-    /// https://confluence.jetbrains.com/display/TCD7/Build+Script+Interaction+with+TeamCity#BuildScriptInteractionwithTeamCity-ReportingBuildProgress for more details
+    /// See <see cref="http://blog.jonnyzzz.name/2012/12/teeamcityservicemessages-v30.html"></see> and 
+    /// <seealso cref="http://confluence.jetbrains.com/display/TCD7/Build+Script+Interaction+with+TeamCity#BuildScriptInteractionwithTeamCity-ReportingBuildProgress for more details"></seealso>
     /// </summary>
     public class TeamCityMessageReporter : StyleCopIssueReporter
     {
-        private readonly ITeamCityWriter writer;
+        private readonly ITeamCityWriter rootWriter;
 
         public TeamCityMessageReporter()
         {
-            this.writer = new TeamCityServiceMessages().CreateWriter();
+            this.rootWriter = new TeamCityServiceMessages().CreateWriter();
         }
 
         public override void ProjectAdded(CsProject project)
         {
-            this.writer.WriteMessage(string.Format("Added Project '{0}' (From: '{1}', Guid: '{2}') for analysis.", project.AssemblyName, Path.Combine(project.Directory, project.File), project.Guid));
+            this.rootWriter.WriteMessage(string.Format("Added Project '{0}' (From: '{1}', Guid: '{2}') for analysis.", project.AssemblyName, Path.Combine(project.Directory, project.File), project.Guid));
         }
 
         public override void Started()
         {
-            this.writer.WriteMessage("StyleCop is analyzing...");
+            this.rootWriter.WriteMessage("StyleCop is analyzing...");
         }
 
         public override void Completed(ExecutionResult result, string tempFileName)
         {
             this.ReportResults(result);
 
-            this.ReportConclusion(result);
+            this.PublishConclusion(result);
         }
 
         private void ReportResults(ExecutionResult result)
@@ -51,7 +49,7 @@ namespace StyleCopCmd.Reporter.TeamCity
 
             var topLevelGrouping = violations.GroupBy(v => v.Violation.SourceCode.Project.Location);
 
-            using (var resultsBlock = this.writer.OpenBlock("Results"))
+            using (var resultsBlock = this.rootWriter.OpenBlock("Results"))
             {
                 foreach (var violationsInProject in topLevelGrouping)
                 {
@@ -62,13 +60,13 @@ namespace StyleCopCmd.Reporter.TeamCity
                     // TC: Open new Suite for each Project
                     using (var suite = resultsBlock.OpenTestSuite(displayNameForGroup))
                     {
-                        this.PublishViolation(violationsInProject, projectPath, suite);
+                        this.PublishViolations(violationsInProject, projectPath, suite);
                     }
                 }
             }
         }
 
-        private void PublishViolation(IEnumerable<ViolationEventArgs> nameSpaceInProjectGroup, string projectPath, ITeamCityTestsSubWriter nameSpaceAsSuite)
+        private void PublishViolations(IEnumerable<ViolationEventArgs> nameSpaceInProjectGroup, string rootPath, ITeamCityTestsSubWriter suiteWriter)
         {
             foreach (var violation in nameSpaceInProjectGroup.OrderBy(v => v.SourceCode.Path).ThenBy(v => v.LineNumber))
             {
@@ -81,29 +79,29 @@ namespace StyleCopCmd.Reporter.TeamCity
                 var path = violation.SourceCode.Path + ":" + violation.LineNumber;
                 var ruleUrl = string.Format("http://www.stylecop.com/docs/{0}.html", styleCopId);
 
-                var testFileName = violation.SourceCode.Path.Replace(projectPath, string.Empty).Substring(1);
+                var testFileName = violation.SourceCode.Path.Replace(rootPath, string.Empty).Substring(1);
                 var testName = styleCopId + "-" + ruleName + "(\"" + testFileName.Replace(".cs", ".cs") + ":" + violation.LineNumber + "\")";
                 
-                using (var test = nameSpaceAsSuite.OpenTest(testName))
+                using (var test = suiteWriter.OpenTest(testName))
                 {
                     test.WriteFailed(string.Format("{0}: {1} ({2})", styleCopId, text, styleCopNameSpace), string.Format("Reason: {0}\n\nFile: {1}\n\nSee: {2} for further explanation", detail, path, ruleUrl));
                 }
             }
         }
 
-        private void ReportConclusion(ExecutionResult result)
+        private void PublishConclusion(ExecutionResult result)
         {
-            this.writer.WriteBuildStatistics("StyleCopWarningsCount", result.WarningsCount.ToString(CultureInfo.InvariantCulture));
-            this.writer.WriteBuildStatistics("StyleCopErrorsCount", result.ErrorsCount.ToString(CultureInfo.InvariantCulture));
+            this.rootWriter.WriteBuildStatistics("StyleCopWarningsCount", result.WarningsCount.ToString(CultureInfo.InvariantCulture));
+            this.rootWriter.WriteBuildStatistics("StyleCopErrorsCount", result.ErrorsCount.ToString(CultureInfo.InvariantCulture));
 
             if (result.HasErrors || result.HasWarnings)
             {
-                this.writer.WriteError(
+                this.rootWriter.WriteError(
                     string.Format("StyleCop failed with {0} Errors and {1} Warnings", result.ErrorsCount, result.WarningsCount));
             }
             else
             {
-                this.writer.WriteMessage("StyleCop completed sucessfully.");
+                this.rootWriter.WriteMessage("StyleCop completed sucessfully.");
             }
         }
     }
